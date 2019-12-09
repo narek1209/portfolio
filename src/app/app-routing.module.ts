7b80612828b900b642b017fb20696e7210e01352
narek <narek@allunited.nl>
2019-12-09 18:52:57 +0100

    finished navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
